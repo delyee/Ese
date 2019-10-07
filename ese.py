68b64502b37b8f02771e1f52c7f7fd87a86980b2
delyee <delyee0@gmail.com>
2019-10-07 05:05:18 +0300

    add colorama and termcolor

--- ese.py
+++ ese.py
@@ -1,19 +1,39 @@
 from sys import argv
 from collections import Counter
+from colorama import Back
+from termcolor import colored
 
-CHAR_SPLIT = argv[2]
+def otherSplit(s):
+    _chars = [' ', '+', '=', '/', "%"]
+    _tmp = []
+    for _char in _chars:
+        for i in s.split(_char):
+            _tmp.append(i.strip())
+    return _tmp
+
+def check(s):
+    _evil = ['eval', 'base64_decode', 'str_rot13', 'gzinflate', 'gzuncompress',
+            'strrev', 'gzdecode', '$GLOBALS', 'Array', '<?php']
+    for _ in _evil:
+        if _ in s:
+            return True
 
 with open(argv[1]) as f:
     queue = []
     for line in f.readlines():
-        for split_line in line.split(CHAR_SPLIT):
+        for split_line in otherSplit(line):
             ## 11 - experimental empirical value
-            if len(split_line) > 11:
-                queue.append(split_line.strip())
+            if len(split_line) > 11 and len(split_line) < 128:
+                queue.append(split_line)
 
 count = Counter()
 for line in queue:
     count[line] += 1
 
-for s, _ in count.most_common(3):
-    print(s)
+for s, c in count.most_common(50):
+    if check(s):
+        print(colored(s, 'white', 'on_red'))
+    else:
+        print(s)
+
+# killmeforthiscode ._.
